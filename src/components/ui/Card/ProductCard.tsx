bc34d89085905faf994270c99173edadc04d65a0
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-02-04 08:44:01 +0700

    Flash Sale Responsive

--- src/components/ui/Card/ProductCard.tsx
+++ src/components/ui/Card/ProductCard.tsx
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
   return (
-    <div className='bg-white rounded-2xl max-w-sm'>
+    <Link to={'/'}>
+        <div className='bg-white rounded-2xl'>
         <div className='relative'>
             <img src="https://image.hsv-tech.io/600x600/bbx/common/bb5a2a0e-fce9-41d9-b1ca-1da7d5cda3f3.webp" alt="" className='object-cover rounded-t-2xl max-w-[100%]'/>
             <img src="https://image.hsv-tech.io/400x0/bbx/common/9d0ffd41-19f9-4c1f-9aab-31c7662f058e.webp" alt="" className='absolute bottom-0'/>
-            <Link to={'/'}>
-                <FavoriteBorderIcon sx={{ fontSize: 25, position: 'absolute', top: 15, right: 15 }}/>
-            </Link>
+            <FavoriteBorderIcon sx={{ fontSize: 25, position: 'absolute', top: 15, right: 10, cursor: 'pointer' }}/>
+            <div className='absolute top-[10px] left-[10px]'>
+                <div className='bg-black text-white rounded-full w-[45px] h-[45px] flex items-center justify-center text-sm mb-3'>MỚI</div>
+                <div className='bg-black text-white rounded-full w-[45px] h-[45px] flex items-center justify-center text-sm'>20%</div>
+            </div>
         </div>
         <div className='text-center p-[14px]'>
-            <Link to={'/'}>
-                <div className='text-sm pb-[8px] font-semibold'>THE FACE SHOP</div>
-            </Link>
+            <div className='text-sm pb-[8px] font-semibold'>THE FACE SHOP</div>
             <div className='text-xs overflow-hidden line-clamp-2'>[Mua 5 tặng 7] Combo Mặt Nạ Giấy THE FACE SHOP Cấp Ẩm Real Nature Face Mask</div>
             <div className='text-sm font-bold py-[10px]'>210.000 đ</div>
             <div className='flex justify-center items-center gap-[5px]'>
@@ -28,6 +29,7 @@ const ProductCard = () => {
             </div>
         </div>
     </div>
+    </Link>
   )
 }
 
--- src/components/ui/Carousel/FlashSaleCarousel.tsx
+++ src/components/ui/Carousel/FlashSaleCarousel.tsx
@@ -1,74 +1,104 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/pagination'; 
+import 'swiper/css/pagination';
 import ProductCard from '../Card/ProductCard';
 import { Navigation } from 'swiper/modules';
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 
 const FlashSaleCarousel = () => {
   return (
-    <div className='bg-[#7233A2] m-auto mt-10 w-[90%] p-[40px] rounded-3xl relative'>
-        <div className='flex justify-between items-center'>
-            <img src="https://image.hsv-tech.io/1920x0/bbx/common/dcfcdba0-ef41-4a57-9936-1d563ed63889.webp" alt="" className='object-cover max-w-[20%]'/>
-                <div className='flex bg-white px-[12px] py-[8px] justify-start rounded-3xl w-auto items-center'>
-                  <div className='px-[8px] flex items-center'>
-                    <div className='mr-[4px] text-[#020c6b] leading-[24px]'>00</div>
-                    <div className='text-[#020c6b] text-sm'>ngày</div>
-                  </div>
-                  <div className='text-sm text-[#020c6b]'>|</div>
-                  <div className='px-[8px] flex items-center'>
-                    <div className='mr-[4px] text-[#020c6b] leading-[24px]'>01</div>
-                    <div className='text-[#020c6b] text-sm'>giờ</div>
-                  </div>
-                  <div className='text-sm text-[#020c6b]'>|</div>
-                  <div className='px-[8px] flex items-center'>
-                    <div className='mr-[4px] text-[#020c6b] leading-[24px]'>20</div>
-                    <div className='text-[#020c6b] text-sm'>phút</div>
-                  </div>
-                  <div className='text-sm text-[#020c6b]'>|</div>
-                  <div className='px-[8px] flex items-center'>
-                    <div className='mr-[4px] text-[#020c6b] leading-[24px]'>30</div>
-                    <div className='text-[#020c6b] text-sm'>giây</div>
-                  </div>
-                </div>
-            <button className='px-[20px] py-[12px] h-auto bg-white text-[#d20062] rounded-lg font-medium'>Xem tất cả</button>
+    <div className='bg-[#7233A2] m-auto mt-10 w-[90%] px-[40px] py-[35px] rounded-3xl relative saleSwiper'>
+      <div className='flex items-center w-full max-md:flex-col max-md:gap-4'>
+        <div className='flex-[3] flex items-center [@media(max-width:820px)]:flex-[5] max-md:flex-none max-md:w-full max-md:justify-center'>
+          <img src="https://image.hsv-tech.io/1920x0/bbx/common/dcfcdba0-ef41-4a57-9936-1d563ed63889.webp" alt="" className='object-cover max-w-[80%] max-md:max-w-[100%]' />
         </div>
-        <div className='relative'>
-          <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            navigation={true}
-            modules={[Navigation]}
-            className="saleSwiper mt-10"
-          >
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <ProductCard/>
-            </SwiperSlide>
-          </Swiper>
+        <div className='flex-[4] flex justify-start [@media(max-width:820px)]:flex-[5] [@media(max-width:820px)]:justify-end max-md:flex-none max-md:justify-center'>
+          <div className='flex bg-white px-[12px] py-[8px] rounded-3xl w-auto items-center'>
+            <div className='px-[8px] flex items-center'>
+              <div className='mr-[4px] text-[#020c6b] leading-[24px]'>00</div>
+              <div className='text-[#020c6b] text-sm'>ngày</div>
+            </div>
+            <div className='text-sm text-[#020c6b]'>|</div>
+            <div className='px-[8px] flex items-center'>
+              <div className='mr-[4px] text-[#020c6b] leading-[24px]'>01</div>
+              <div className='text-[#020c6b] text-sm'>giờ</div>
+            </div>
+            <div className='text-sm text-[#020c6b]'>|</div>
+            <div className='px-[8px] flex items-center'>
+              <div className='mr-[4px] text-[#020c6b] leading-[24px]'>20</div>
+              <div className='text-[#020c6b] text-sm'>phút</div>
+            </div>
+            <div className='text-sm text-[#020c6b]'>|</div>
+            <div className='px-[8px] flex items-center'>
+              <div className='mr-[4px] text-[#020c6b] leading-[24px]'>30</div>
+              <div className='text-[#020c6b] text-sm'>giây</div>
+            </div>
+          </div>
         </div>
+        <div className='flex-[3] flex justify-end [@media(max-width:820px)]:hidden'>
+          <button className='px-[20px] py-[12px] h-auto bg-white text-[#d20062] rounded-lg font-medium cursor-pointer'>Xem tất cả</button>
+        </div>
+      </div>
+      <div className='relative'>
+        <Swiper
+          spaceBetween={20}
+          navigation={{
+            nextEl: '.flash-next',
+            prevEl: '.flash-prev',
+          }}
+          breakpoints={{
+            0: {
+              slidesPerView: 2
+            },
+            640: {
+              slidesPerView: 2
+            },
+            768: {
+              slidesPerView: 3
+            },
+            1024: {
+              slidesPerView: 3
+            },
+            1280: {
+              slidesPerView: 4
+            },
+          }}
+          modules={[Navigation]}
+          className="mt-10"
+        >
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductCard />
+          </SwiperSlide>
+        </Swiper>
+        <div className="flash-prev absolute w-[50px] h-[50px] rounded-full flex items-center justify-center cursor-pointer bg-white left-[-65px] top-[35%] shadow-[0_54px_55px_rgba(0,0,0,0.25),0_-12px_30px_rgba(0,0,0,0.12),0_4px_6px_rgba(0,0,0,0.12),0_12px_13px_rgba(0,0,0,0.17),0_-3px_5px_rgba(0,0,0,0.09)]">
+          <ArrowBackIosNewOutlinedIcon sx={{ fontSize: 18 }}/>
+        </div>
+        <div className="flash-next absolute w-[50px] h-[50px] rounded-full flex items-center justify-center cursor-pointer bg-white right-[-65px] top-[35%] shadow-[0_54px_55px_rgba(0,0,0,0.25),0_-12px_30px_rgba(0,0,0,0.12),0_4px_6px_rgba(0,0,0,0.12),0_12px_13px_rgba(0,0,0,0.17),0_-3px_5px_rgba(0,0,0,0.09)]">
+          <ArrowForwardIosOutlinedIcon sx={{ fontSize: 18 }}/>
+        </div> 
+      </div>
+      <div className='flex justify-center mt-[35px]'>
+        <button className='px-[30px] py-[12px] h-auto bg-white text-[#d20062] rounded-lg font-medium cursor-pointer [@media(min-width:820px)]:hidden'>Xem tất cả</button>
+      </div>
     </div>
   )
 }
